2e81f782b130721196bdffa27cfe18366361bee5
n1ce <n1ce37333@gmail.com>
2024-06-01 00:03:03 +0800

    add storage

--- src/device/src/storage.rs
+++ src/device/src/storage.rs
@@ -1,36 +1,197 @@
-use std::{io, path::Path};
+use std::{fs::Permissions, io, os::unix::fs::PermissionsExt, path::Path};
 
-use serde::Deserialize;
-use serde_json::Value;
 use tokio::{
-    fs::{self, File, OpenOptions},
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt},
+    fs::{self, OpenOptions},
+    io::{AsyncReadExt, AsyncWriteExt},
 };
 
 static ROOT_DIR: &str = "./storage";
 static FILE_NAME: &str = "data";
+static DELIMITER: char = '-';
 
 pub(crate) async fn insert_device(id: u64, data: String) -> Result<(), io::Error> {
-    let device_path = Path::new(ROOT_DIR).join(FILE_NAME);
-    let mut file = match fs::try_exists(&device_path).await? {
-        true => OpenOptions::new().append(true).open(&device_path).await?,
-        false => {
-            OpenOptions::new()
-                .read(true)
-                .create_new(true)
-                .write(true)
-                .mode(0o466)
-                .open(&device_path)
-                .await?
-        }
-    };
-    file.write(format!("{}-{}\n", id, data).as_bytes()).await?;
-    Ok(())
+    insert(Path::new(ROOT_DIR).join(FILE_NAME), id, data).await?;
+    fs::create_dir(Path::new(ROOT_DIR).join(id.to_string())).await
 }
 
-pub(crate) async fn get_devices() -> Result<Vec<(u64, String)>, io::Error> {
-    let device_path = Path::new(ROOT_DIR).join(FILE_NAME);
-    let mut file = OpenOptions::new().read(true).open(device_path).await?;
+pub(crate) async fn read_devices() -> Result<Vec<(u64, String)>, io::Error> {
+    read(Path::new(ROOT_DIR).join(FILE_NAME)).await
+}
+
+pub(crate) async fn update_device(id: u64, data: String) -> Result<(), io::Error> {
+    update(Path::new(ROOT_DIR).join(FILE_NAME), id, data).await
+}
+
+pub(crate) async fn delete_device(id: u64) -> Result<(), io::Error> {
+    delete(Path::new(ROOT_DIR).join(FILE_NAME), id).await?;
+    fs::remove_dir(Path::new(ROOT_DIR).join(id.to_string())).await
+}
+
+pub(crate) async fn insert_group(
+    device_id: u64,
+    group_id: u64,
+    data: String,
+) -> Result<(), io::Error> {
+    insert(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(FILE_NAME),
+        group_id,
+        data,
+    )
+    .await?;
+
+    fs::create_dir(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(group_id.to_string()),
+    )
+    .await
+}
+
+pub(crate) async fn read_groups(device_id: u64) -> Result<Vec<(u64, String)>, io::Error> {
+    read(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(FILE_NAME),
+    )
+    .await
+}
+
+pub(crate) async fn update_group(
+    device_id: u64,
+    group_id: u64,
+    data: String,
+) -> Result<(), io::Error> {
+    update(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(FILE_NAME),
+        group_id,
+        data,
+    )
+    .await
+}
+
+pub(crate) async fn delete_group(device_id: u64, group_id: u64) -> Result<(), io::Error> {
+    delete(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(FILE_NAME),
+        group_id,
+    )
+    .await
+}
+
+pub(crate) async fn insert_points(
+    device_id: u64,
+    group_id: u64,
+    datas: &[(u64, String)],
+) -> Result<(), io::Error> {
+    let path = Path::new(ROOT_DIR)
+        .join(device_id.to_string())
+        .join(group_id.to_string())
+        .join(FILE_NAME);
+    let mut file = match fs::try_exists(&path).await? {
+        true => OpenOptions::new().append(true).open(&path).await?,
+        false => {
+            let file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(&path)
+                .await?;
+            file.set_permissions(Permissions::from_mode(0o666)).await?;
+            file
+        }
+    };
+
+    for (id, data) in datas {
+        file.write(format!("{}{}{}\n", id, DELIMITER, data).as_bytes())
+            .await?;
+    }
+    file.flush().await
+}
+
+pub(crate) async fn read_points(
+    device_id: u64,
+    group_id: u64,
+) -> Result<Vec<(u64, String)>, io::Error> {
+    read(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(group_id.to_string())
+            .join(FILE_NAME),
+    )
+    .await
+}
+
+pub(crate) async fn update_point(
+    device_id: u64,
+    group_id: u64,
+    point_id: u64,
+    data: String,
+) -> Result<(), io::Error> {
+    update(
+        Path::new(ROOT_DIR)
+            .join(device_id.to_string())
+            .join(group_id.to_string())
+            .join(FILE_NAME),
+        point_id,
+        data,
+    )
+    .await
+}
+
+pub(crate) async fn delete_point(
+    device_id: u64,
+    group_id: u64,
+    points_ids: Vec<u64>,
+) -> Result<(), io::Error> {
+    let path = Path::new(ROOT_DIR)
+        .join(device_id.to_string())
+        .join(group_id.to_string())
+        .join(FILE_NAME);
+    let mut file = OpenOptions::new().read(true).open(&path).await?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).await?;
+
+    let mut lines: Vec<&str> = buf.split("\n").collect();
+    lines.retain(|line| match line.find(DELIMITER) {
+        Some(pos) => points_ids.contains(&line[..pos].parse::<u64>().expect("文件")),
+        None => false,
+    });
+
+    let buf = lines.join("\n");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&path)
+        .await?;
+
+    file.write(buf.as_bytes()).await?;
+    file.flush().await
+}
+
+async fn insert(path: impl AsRef<Path>, id: u64, data: String) -> Result<(), io::Error> {
+    let mut file = match fs::try_exists(&path).await? {
+        true => OpenOptions::new().append(true).open(&path).await?,
+        false => {
+            let file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(&path)
+                .await?;
+            file.set_permissions(Permissions::from_mode(0o666)).await?;
+            file
+        }
+    };
+    file.write(format!("{}{}{}\n", id, DELIMITER, data).as_bytes())
+        .await?;
+    file.flush().await
+}
+
+async fn read(path: impl AsRef<Path>) -> Result<Vec<(u64, String)>, io::Error> {
+    let mut file = OpenOptions::new().read(true).open(path).await?;
     let mut buf = String::new();
     file.read_to_string(&mut buf).await?;
 
@@ -47,9 +208,8 @@ pub(crate) async fn get_devices() -> Result<Vec<(u64, String)>, io::Error> {
         .collect())
 }
 
-pub(crate) async fn update_device(id: u64, data: String) -> Result<(), io::Error> {
-    let device_path = Path::new(ROOT_DIR).join(FILE_NAME);
-    let mut file = OpenOptions::new().write(true).open(device_path).await?;
+async fn update(path: impl AsRef<Path>, id: u64, data: String) -> Result<(), io::Error> {
+    let mut file = OpenOptions::new().write(true).open(path).await?;
     let mut buf = String::new();
     file.read_to_string(&mut buf).await?;
 
@@ -64,118 +224,27 @@ pub(crate) async fn update_device(id: u64, data: String) -> Result<(), io::Error
 
     let buf = lines.join("\n");
     file.write_all(buf.as_bytes()).await?;
-
     Ok(())
 }
 
-pub(crate) async fn delete_device(id: u64) -> Result<(), io::Error> {
-    let device_path = Path::new(ROOT_DIR).join(FILE_NAME);
-    let mut file = OpenOptions::new().write(true).open(device_path).await?;
+async fn delete(path: impl AsRef<Path>, id: u64) -> Result<(), io::Error> {
+    let mut file = OpenOptions::new().read(true).open(&path).await?;
     let mut buf = String::new();
     file.read_to_string(&mut buf).await?;
 
     let mut lines: Vec<&str> = buf.split("\n").collect();
-    lines.retain(|line| {
-        let split_pos = line.find('-').expect("数据文件损坏");
-        line[..split_pos].parse::<u64>().expect("文件") != id
+    lines.retain(|line| match line.find(DELIMITER) {
+        Some(pos) => line[..pos].parse::<u64>().expect("文件") != id,
+        None => false,
     });
 
     let buf = lines.join("\n");
-    file.write_all(buf.as_bytes()).await?;
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&path)
+        .await?;
 
-    Ok(())
-}
-
-pub(crate) async fn insert_group(
-    device_id: u64,
-    group_id: u64,
-    data: &[u8],
-) -> Result<(), io::Error> {
-    Ok(())
-}
-
-pub(crate) async fn update_group(
-    device_id: u64,
-    group_id: u64,
-    data: &[u8],
-) -> Result<(), io::Error> {
-    Ok(())
-}
-
-pub(crate) async fn delete_group(device_id: u64, group_id: u64) -> Result<(), io::Error> {
-    Ok(())
-}
-
-pub(crate) async fn insert_points(
-    device_id: u64,
-    group_id: u64,
-    datas: &[(u64, &[u8])],
-) -> Result<(), io::Error> {
-    Ok(())
-}
-
-pub(crate) async fn update_point(
-    device_id: u64,
-    group_id: u64,
-    point_id: u64,
-    data: &[u8],
-) -> Result<(), io::Error> {
-    Ok(())
-}
-
-pub(crate) async fn delete_point(
-    device_id: u64,
-    group_id: u64,
-    points_ids: &[u64],
-) -> Result<(), io::Error> {
-    Ok(())
-}
-
-async fn insert(path: impl AsRef<Path>, record: &[u8]) -> Result<(), io::Error> {
-    let mut file = match fs::try_exists(&path).await? {
-        true => OpenOptions::new().append(true).open(&path).await?,
-        false => File::create(&path).await?,
-    };
-
-    file.write_all(record).await?;
+    file.write(buf.as_bytes()).await?;
     file.flush().await
 }
-
-async fn create_dir(path: impl AsRef<Path>) -> Result<(), io::Error> {
-    match fs::try_exists(&path).await? {
-        true => Ok(()),
-        false => fs::create_dir_all(&path).await,
-    }
-}
-
-async fn read_file(path: impl AsRef<Path>) -> Result<(), io::Error> {
-    todo!()
-}
-
-async fn update(path: impl AsRef<Path>, record: &[u8], id: u64) -> Result<(), io::Error> {
-    todo!()
-}
-
-// async fn delete(path: impl AsRef<Path>, id: u64) -> Result<(), io::Error> {
-//     let mut file = File::open(&path).await?;
-
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents).await?;
-
-//     let mut lines: Vec<&str> = contents.lines().collect();
-//     lines.retain(|line| match serde_json::from_str::<Record>(line) {
-//         Ok(record) => record.id != id,
-//         Err(_) => {
-//             error!("文件已损坏");
-//             true
-//         }
-//     });
-
-//     let new_contents = lines.join("\n");
-
-//     let mut file = File::create(&path).await?;
-//     file.write_all(new_contents.as_bytes()).await?;
-//     file.flush().await?;
-
-//     Ok(())
-// }
