bb70372da33810264b26c45eae3b2ae163f44b93
n1ce <n1ce37333@gmail.com>
2024-07-07 08:11:13 -0400

    deving

--- src/device/src/opcua/group.rs
+++ src/device/src/opcua/group.rs
@@ -1,18 +1,97 @@
-use std::collections::HashMap;
+use std::time::Duration;
 
+use anyhow::{bail, Result};
 use message::MessageBatch;
-use tokio::sync::{broadcast, RwLock};
+use tokio::{
+    select,
+    sync::{broadcast, mpsc, RwLock},
+    time,
+};
+use tracing::{debug, error};
+use types::device::group::CreateGroupReq;
 use uuid::Uuid;
 
 use super::point::Point;
+
+#[derive(Clone)]
+pub(crate) enum Command {
+    Stop(Uuid),
+    Pause,
+    Restart,
+    Update(Uuid, u64),
+    StopAll,
+}
 
 #[derive(Debug)]
 pub struct Group {
     pub id: Uuid,
     pub name: String,
     pub interval: u64,
-    pub points: RwLock<HashMap<Uuid, Point>>,
-    pub device_id: Uuid,
+    pub points: RwLock<Vec<Point>>,
     pub tx: Option<broadcast::Sender<MessageBatch>>,
     pub ref_cnt: usize,
+    pub desc: Option<String>,
+}
+
+impl Group {
+    pub fn new(group_id: Uuid, conf: &CreateGroupReq) -> Result<Self> {
+        if conf.interval == 0 {
+            bail!("group interval must > 0")
+        }
+        Ok(Group {
+            id: group_id,
+            name: conf.name.clone(),
+            interval: conf.interval,
+            points: RwLock::new(vec![]),
+            tx: None,
+            ref_cnt: 0,
+            desc: conf.desc.clone(),
+        })
+    }
+
+    pub fn run(&self, mut cmd_rx: broadcast::Receiver<Command>, read_tx: mpsc::Sender<Uuid>) {
+        let interval = self.interval;
+        let group_id = self.id;
+        tokio::spawn(async move {
+            let mut pause = false;
+            let mut interval = time::interval(Duration::from_millis(interval));
+            loop {
+                select! {
+                    biased;
+                    signal = cmd_rx.recv() => {
+                        match signal {
+                            Ok(cmd) => {
+                                match cmd {
+                                    Command::Stop(id) =>  if id == group_id {
+                                        debug!("group {} stop.", group_id);
+                                        return
+                                    }
+                                    Command::Pause => pause = true,
+                                    Command::Restart => pause = false,
+                                    Command::Update(id, duraion) => {
+                                        if id == group_id {
+                                            interval = time::interval(Duration::from_millis(duraion));
+                                        }
+                                    }
+                                    Command::StopAll => {
+                                        debug!("group {} stop.", group_id);
+                                        return
+                                    }
+                                }
+                            }
+                            Err(e) => error!("group recv cmd signal err :{:?}", e),
+                        }
+                    }
+
+                    _ = interval.tick() => {
+                        if !pause {
+                            if let Err(e) = read_tx.send(group_id).await {
+                                debug!("group send point info err :{}", e);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
--- src/device/src/opcua/mod.rs
+++ src/device/src/opcua/mod.rs
@@ -1,27 +1,40 @@
-use std::sync::{atomic::AtomicBool, Arc, Mutex};
-
+use async_trait::async_trait;
+use bytes::Bytes;
 use common::error::HaliaResult;
 use group::Group;
-use tokio::sync::RwLock;
-use types::device::device::CreateDeviceReq;
+use message::MessageBatch;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use tokio::sync::{broadcast, mpsc, Mutex, RwLock};
+use types::device::{
+    device::{CreateDeviceReq, SearchDeviceItemResp, SearchSinksResp, UpdateDeviceReq},
+    group::{CreateGroupReq, SearchGroupResp, UpdateGroupReq},
+    point::{CreatePointReq, SearchPointResp},
+};
 use uuid::Uuid;
 
 use crate::Device;
 
+pub(crate) const TYPE: &str = "opcua";
 mod group;
 mod point;
 
 struct OpcUa {
     id: Uuid,
+    name: String,
     on: Arc<AtomicBool>,
     err: Arc<AtomicBool>,
     conf: Arc<Mutex<Conf>>,
     groups: Arc<RwLock<Vec<Group>>>,
 }
 
+#[derive(Deserialize, Serialize, Clone)]
 struct Conf {
     url: String,
-    password: Option<Password>,
 }
 
 struct Password {
@@ -30,5 +43,137 @@ struct Password {
 }
 
 pub(crate) fn new(id: Uuid, req: &CreateDeviceReq) -> HaliaResult<Box<dyn Device>> {
-    todo!()
+    let conf: Conf = serde_json::from_value(req.conf.clone())?;
+
+    Ok(Box::new(OpcUa {
+        id,
+        name: req.name.clone(),
+        on: Arc::new(AtomicBool::new(false)),
+        err: Arc::new(AtomicBool::new(false)),
+        conf: Arc::new(Mutex::new(conf)),
+        groups: Arc::new(RwLock::new(vec![])),
+    }))
+}
+
+#[async_trait]
+impl Device for OpcUa {
+    fn get_id(&self) -> Uuid {
+        self.id
+    }
+
+    async fn get_info(&self) -> SearchDeviceItemResp {
+        SearchDeviceItemResp {
+            id: self.id,
+            name: self.name.clone(),
+            r#type: TYPE,
+            on: self.on.load(Ordering::SeqCst),
+            err: self.err.load(Ordering::SeqCst),
+            rtt: 9999,
+            conf: json!(&self.conf.lock().await.clone()),
+        }
+    }
+
+    async fn start(&mut self) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn stop(&mut self) {
+        todo!()
+    }
+
+    async fn update(&mut self, req: &UpdateDeviceReq) -> HaliaResult<()> {
+        todo!()
+    }
+
+    // group
+    async fn create_group(&mut self, group_id: Uuid, req: &CreateGroupReq) -> HaliaResult<()> {
+        match Group::new(group_id, &req) {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
+        }
+    }
+
+    async fn search_groups(&self, page: usize, size: usize) -> HaliaResult<SearchGroupResp> {
+        todo!()
+    }
+
+    async fn update_group(&self, group_id: Uuid, req: UpdateGroupReq) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn delete_group(&self, group_id: Uuid) -> HaliaResult<()> {
+        todo!()
+    }
+
+    // points
+    async fn create_point(
+        &self,
+        group_id: Uuid,
+        point_id: Uuid,
+        req: CreatePointReq,
+    ) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn search_point(
+        &self,
+        group_id: Uuid,
+        page: usize,
+        size: usize,
+    ) -> HaliaResult<SearchPointResp> {
+        todo!()
+    }
+
+    async fn update_point(
+        &self,
+        group_id: Uuid,
+        point_id: Uuid,
+        req: &CreatePointReq,
+    ) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn write_point_value(
+        &self,
+        group_id: Uuid,
+        point_id: Uuid,
+        value: serde_json::Value,
+    ) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn delete_points(&self, group_id: &Uuid, point_ids: &Vec<Uuid>) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn subscribe(
+        &mut self,
+        group_id: &Uuid,
+    ) -> HaliaResult<broadcast::Receiver<MessageBatch>> {
+        todo!()
+    }
+
+    async fn unsubscribe(&mut self, group_id: Uuid) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn create_sink(&self, sink_id: Uuid, req: &Bytes) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn search_sinks(&self, page: usize, size: usize) -> SearchSinksResp {
+        todo!()
+    }
+
+    async fn update_sink(&self, sink_id: Uuid, req: &Bytes) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn delete_sink(&self, sink_id: Uuid) -> HaliaResult<()> {
+        todo!()
+    }
+
+    async fn publish(&mut self, sink_id: &Uuid) -> HaliaResult<mpsc::Sender<MessageBatch>> {
+        todo!()
+    }
 }
